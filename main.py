d178e780373808016400a3c583fc5f30e617c83a
Ayush Suman <ayush231suman@gmail.com>
2021-07-19 19:06:34 +0530

    trying out openpyxl

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
 from fuzzywuzzy import process, fuzz
+import sys
+from openpyxl import Workbook
+from openpyxl import load_workbook
+
+
+def excel():
+    filename = sys.argv[0]
+    filename2 = sys.argv[1]
+
+    std_workbook = load_workbook(filename=filename, read_only=True)
+    workbook = load_workbook(filename=filename2, read_only=True)
+
+    std_sheet = std_workbook.active
+
+    workbook_result = Workbook()
+    sheet_result = workbook_result.active
+
+    sheet_result["A1"] = "hello"
+    sheet_result["B1"] = "world!"
+
+    # Using the values_only because you just want to return the cell value
+    for row in sheet_result.iter_rows(min_row=2, values_only=True):
+        print(row)
+
+    workbook_result.save(filename="combined_result.xlsx")
+
 
 sample_words = ["date of birth", "birth_date", "birth date", "ayush suman", "date of join"]
 reference = ["ayush", "joining date", "birth day"]
@@ -10,16 +36,19 @@ reference = ["ayush", "joining date", "birth day"]
 
 # algorithm to identify closest match
 def loop_over_for_max_score():
+    closest_match_dict = {}
     for i in sample_words:
         max_score = 0
         closest_match = ""
         for j in reference:
             score = fuzz.token_sort_ratio(i, j)
-            print(i, j, score)
+            # print(i, j, score)
             if max_score < score:
                 max_score = score
                 closest_match = j
-        print(i, "matches closest with", closest_match)
+        # print(i, "matches closest with", closest_match)
+        closest_match_dict[closest_match] = i
+    return closest_match_dict
 
 
 # Output :
@@ -47,9 +76,12 @@ def loop_over_for_max_score():
 
 # algorithm using process extractOne
 def extract_using_process():
+    closest_match_dict = {}
     for i in sample_words:
         closest_match = process.extractOne(i, reference, scorer=fuzz.token_sort_ratio, score_cutoff=50)
-        print(i, "matches closes with", closest_match.__getitem__(0), closest_match.__getitem__(1))
+        closest_match_dict[closest_match.__getitem__(0)] = i
+        # print(i, "matches closes with", closest_match.__getitem__(0), closest_match.__getitem__(1))
+    return closest_match_dict
 
 
 # Output:
@@ -60,5 +92,7 @@ def extract_using_process():
 # date of join matches closes with joining date 75
 
 if __name__ == "__main__":
-    extract_using_process()
-    # loop_over_for_max_score()
+    closest = extract_using_process()
+    print(closest)
+    closest = loop_over_for_max_score()
+    print(closest)
